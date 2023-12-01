ab3b510f0608c4ea69121a55a2600f136fc54ff0
Tom Wallace <tom.wallace@ticketmaster.com>
2023-12-01 11:57:47 -0500

    Day 1 puzzles and tests.

---
+++ AdventOfCode2023.CSharp/Day01/DayOne.cs
@@ -0,0 +1,105 @@
+﻿using AdventOfCode2023.CSharp.Utility;
+
+namespace AdventOfCode2023.CSharp.Day01;
+
+public class DayOne : IAdventProblemSet
+{
+    private Dictionary<string, string> numbers = new Dictionary<string, string>()
+    {
+        {"one", "1"}, {"two", "2"}, {"three", "3" }, {"four", "4"}, {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+    };
+    
+    /// <inheritdoc />
+    public string Description()
+    {
+        return "Trebuchet?!";
+    }
+
+    /// <inheritdoc />
+    public int SortOrder()
+    {
+        return 1;
+    }
+
+    /// <inheritdoc />
+    public string PartA()
+    {
+        var filePath = @"Day01\DayOneInput.txt";
+        var sum = SumLinesOfFirstAndLastDigits(filePath, false);
+
+        return sum.ToString();
+    }
+
+    /// <inheritdoc />
+    public string PartB()
+    {
+        var filePath = @"Day01\DayOneInput.txt";
+        var sum = SumLinesOfFirstAndLastDigits(filePath, true);
+
+        return sum.ToString();
+    }
+
+    internal int SumLinesOfFirstAndLastDigits(string filePath, bool useWords)
+    {
+        var lines = FileUtility.ParseFileToList(filePath, line => useWords ? ProcessLineAsWords(line) : ProcessLine(line));
+
+        return lines.Sum();
+    }
+
+    internal int ProcessLine(string line)
+    {
+        var charInts = line.ToCharArray().Where(c => char.IsDigit(c)).ToList();
+        return int.Parse($"{charInts.First()}{charInts.Last()}");
+    }
+
+
+    internal int ProcessLineAsWords(string line)
+    {
+        var lowestIndex = 99999;
+        var lowestNum = "";
+        var highestIndex = -1;
+        var highestNum = "";
+
+        for( int i = 0; i < line.Length; i++ )
+        {
+            if (!char.IsDigit(line[i]))
+                continue;
+
+            if (i <= lowestIndex)
+            {
+                lowestIndex = i;
+                lowestNum = line[i].ToString();
+            }
+
+            if (i >= highestIndex)
+            {
+                highestIndex = i;
+                highestNum = line[i].ToString();
+            }
+        }
+
+        foreach(var d in numbers)
+        {
+            var indx = line.IndexOf(d.Key);
+            if (indx == -1)
+                continue;
+
+            if (indx <= lowestIndex)
+            {
+                lowestIndex = indx;
+                lowestNum = d.Value;
+            }
+
+            var highIndx = line.LastIndexOf(d.Key);
+            if (highIndx >= highestIndex)
+            {
+                highestIndex = highIndx;
+                highestNum = d.Value;
+            }
+        }
+
+        return int.Parse($"{lowestNum}{highestNum}");
+    }
+
+    
+}
--- AdventOfCode2023.CSharpTests/Day01Tests.cs
+++ AdventOfCode2023.CSharpTests/Day01Tests.cs
@@ -1,51 +1,77 @@
-﻿using AdventOfCode2023.CSharp.OldDay01;
-using AdventOfCode2023.CSharp.Utility;
+﻿using AdventOfCode2023.CSharp.Day01;
 
 namespace AdventOfCode2022.CSharp.Tests;
 
-public class OldDayOneTests
+public class DayOneTests
 {
-    [Fact]
-    public void CalculateCalorieTotals()
+    [Theory]
+    [InlineData("1abc2",12)]
+    [InlineData("pqr3stu8vwx", 38)]
+    [InlineData("a1b2c3d4e5f", 15)]
+    [InlineData("treb7uchet", 77)]
+    public void ProcessLine(string input, int expected)
     {
-        string filePath = @"OldDay01\OldDayOneTestInputA.txt";
-        List<string> data = FileUtility.ParseFileToList(filePath);
+        var sut = new DayOne();
+        var result = sut.ProcessLine(input);
 
-        var sut = new OldDayOne();
-        var result = sut.CalculateCalorieTotals(data);
-
-        Assert.Equal(5, result.Count);
-        Assert.Equal(24000, result.Max());
-        Assert.Equal(4000, result.Min());
+        Assert.Equal(expected, result);
     }
 
     [Fact]
-    public void TotalCaloriesTopThreeElves()
+    public void SumLinesOfFirstAndLastDigits()
     {
-        string filePath = @"OldDay01\OldDayOneTestInputA.txt";
-        List<string> data = FileUtility.ParseFileToList(filePath);
+        string filePath = @"Day01\DayOneTestInputA.txt";
 
-        var sut = new OldDayOne();
-        var result = sut.TotalCaloriesTopThreeElves(data);
+        var sut = new DayOne();
+        var result = sut.SumLinesOfFirstAndLastDigits(filePath, false);
 
-        Assert.Equal(45000, result);
+        Assert.Equal(142, result);
+    }
+
+    [Theory]
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
+    [InlineData("1234oneight", 18)]
+    [InlineData("7one718onegfqtdbtxfcmd", 71)]
+    public void ProcessLineAsWords(string input, int expected)
+    {
+        var sut = new DayOne();
+        var result = sut.ProcessLineAsWords(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void SumLinesOfFirstAndLastDigits_WithWords()
+    {
+        string filePath = @"Day01\DayOneTestInputB.txt";
+
+        var sut = new DayOne();
+        var result = sut.SumLinesOfFirstAndLastDigits(filePath, true);
+
+        Assert.Equal(281, result);
     }
 
     [Fact]
     public void PartA_Actual()
     {
-        var sut = new OldDayOne();
+        var sut = new DayOne();
         var result = sut.PartA();
 
-        Assert.Equal("71924", result);
+        Assert.Equal("55108", result);
     }
 
     [Fact]
     public void PartB_Actual()
     {
-        var sut = new OldDayOne();
+        var sut = new DayOne();
         var result = sut.PartB();
 
-        Assert.Equal("210406", result);
+        Assert.Equal("56324", result);
     }
 }
